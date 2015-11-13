49de06cd3589ca7b24e8937df8477e8f12154e72
Jean-Sébastien Goupil <Jean-Sbastien Goupil>
2015-11-12 21:41:42 -0800

    First

---
+++ TestRuby/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestRuby.Startup))]
+namespace TestRuby
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
